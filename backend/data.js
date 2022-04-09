0a9362add1e7efc8025f9a9ca050cc7b392b47c6
Brendan <brendan.aucoin@ryerson.ca>
2022-04-09 19:30:25 -0400

    added backend to backend branch

---
+++ backend/data.js
@@ -0,0 +1,35 @@
+export default {
+    items:[
+        {
+        slug: '1',
+        pic: '/pictures/messi.jpg',
+        league: 'LaLiga',
+        name: 'Messi',
+        team: 'FC Barcelona',
+        price: 120
+        },
+        {
+            slug: '2',
+            pic: '/pictures/ronaldo.png',
+            league: 'PremierLeague',
+            name: 'Cristiano Ronaldo',
+            team: 'Manchester United',
+            price: 130
+        },
+        {
+            slug: '3',
+            pic: '/pictures/felix.png',
+            league: 'Laliga',
+            name: 'Joao Felix',
+            team: 'Atletico Madrid',
+            price: 100
+        },
+        {
+            slug: '4',
+            pic: '/pictures/haaland.png',
+            league: 'Bundesliga',
+            name: 'Erling Haaland',
+            team: 'Borussia Dortmund',
+            price: 110
+        }]
+}
